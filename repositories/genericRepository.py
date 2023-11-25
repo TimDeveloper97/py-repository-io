1a83e4a25fa69da67c7a2c7a51f1dff6f056829d
TimDeveloper97 <timdeveloper97@gmail.com>
2023-11-25 22:35:02 +0700

    generic repository

--- repositories/genericRepository.py
+++ repositories/genericRepository.py
@@ -1,77 +1,99 @@
 import os
 import json
 import xmlRepository as xml
+import uuid
+import shutil
 
 
 class genericRepository:
 
-    def __init__(self, root="root") -> None:
+    def __init__(self, root, commany, table=None) -> None:
         self.root = root
+        self.commany = commany
+        self.table = table
+        self._xml = xml.xmlRepository()
 
-    def add(folderId, obj):
+    @property
+    def pathFolder(self):
+        if self.table != None:
+            return self.root + f"\\{self.commany}\\{self.table}\\"
+        else:
+            return self.root + f"\\{self.commany}\\"
+
+    def add(self, obj):
         """
         Create file with content obj in folderId.
 
         Parameters:
-        - folderId (string): id of folder to write file.
         - obj (string): object to write.
 
         Returns: TBD.
         """
         try:
-            print("")
+            if self.pathFolder and os.path.exists(self.pathFolder):
+                if not obj.id:
+                    obj.id = str(uuid.uuid4())
+
+                self._xml.createById(self.pathFolder, obj)
         except Exception as ex:
             print("Error: ", ex)
         return None
 
-    def get(folderId, id):
+    def get(self, id):
         """
         Get file with id in folderId.
 
         Parameters:
-        - folderId (string): id of folder to write file.
         - id (string): id of file xml.
 
         Returns: TBD.
         """
         try:
-            print("")
+            if self.exist(self.pathFolder, id):
+                pathFile = self.pathFolder + f"{id}.xml"
+
+                xmlFile = self._xml.read(pathFile)
+                return xmlFile
         except Exception as ex:
             print("Error: ", ex)
         return None
 
-    def getAll(folderId):
+    def getAll(self):
         """
         Get all file in folderId.
-
-        Parameters:
-        - folderId (string): id of folder to write file.
 
         Returns: TBD.
         """
         try:
-            print("")
+            if self.exist(self.pathFolder):
+                xmlFiles = self._xml.readAll(self.pathFolder)
+                return xmlFiles
         except Exception as ex:
             print("Error: ", ex)
         return None
 
-    def exist(folderId, id):
+    def exist(self, pathFolder, id=None):
         """
         Checked exist file with id in folderId.
 
         Parameters:
-        - folderId (string): id of folder to write file.
+        - pathFolder (string): pathFolder.
         - id (string): id of file xml.
 
         Returns: TBD.
         """
         try:
-            print("")
+            if not os.path.exists(pathFolder):
+                return False
+
+            if id != None:
+                pathFile = pathFolder + f"{id}.xml"
+                return os.path.exists(pathFile)
         except Exception as ex:
             print("Error: ", ex)
-        return None
+        return True
 
-    def update(folderId, obj):
+    def update(self, obj):
         """
         Update exist file with obj.id in folderId.
 
@@ -82,12 +104,14 @@ class genericRepository:
         Returns: TBD.
         """
         try:
-            print("")
+            if self.exist(self.pathFolder):
+                pathFile = self.pathFolder + f"{id}.xml"
+                self._xml.updateById(pathFile)
         except Exception as ex:
             print("Error: ", ex)
         return None
 
-    def delete(folderId, id):
+    def delete(self, id):
         """
         Delete exist file with id in folderId.
 
@@ -98,12 +122,14 @@ class genericRepository:
         Returns: TBD.
         """
         try:
-            print("")
+            if self.exist(self.pathFolder):
+                pathFile = self.pathFolder + f"{id}.xml"
+                self._xml.delete(pathFile)
         except Exception as ex:
             print("Error: ", ex)
         return None
 
-    def deleteAll(folderId):
+    def deleteAll(self, folderId):
         """
         Delete folderId.
 
@@ -113,7 +139,8 @@ class genericRepository:
         Returns: TBD.
         """
         try:
-            print("")
+            if self.exist(self.pathFolder):
+                shutil.rmtree(self.pathFolder)
         except Exception as ex:
             print("Error: ", ex)
         return None
